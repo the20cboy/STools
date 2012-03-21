3fe8a62d6a365da5830d344986c19bfc908046ef
MinSub Jung <MinSub.Jung@me.com>
2012-03-21 10:02:48 +0900

    S-Tools Committed. is Form Designer
    MinSub Jung <MinSub.Jung@me.com>

---
+++ STools/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using log4net;
+
+using STools.Core;
+
+namespace STools
+{
+    public partial class Form1 : Form
+    {
+        protected readonly static ILog _logger =LogManager.GetLogger(typeof(Program));
+        private STool _sTools = null;
+
+        public Form1()
+        {
+            SToolSettings settings = new SToolSettings();
+            settings.StartupPath = Application.StartupPath;
+            settings.Logger = _logger;
+
+            _sTools = new STool(settings);
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            /// STool Initialize
+            loadProgress.Style = ProgressBarStyle.Continuous;
+            lbMessage.Text = "S-Tool Started.";
+
+            lbMessage.Text = "Initialize S-Tools";
+            if (!_sTools.Initialize())
+            {
+                /// Initialize Failed.
+                MessageBox.Show("S-Tools Initialize Failed\nPlease Check System Log Files.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                this.Close();
+            }
+        }
+    }
+}
---
+++ STools/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.Reflection;
+using System.IO;
+
+namespace STools
+{
+    static class Program
+    {
+        /// <summary>
+        /// 해당 응용 프로그램의 주 진입점입니다.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            //AppDomain.CurrentDomain.AssemblyResolve += new ResolveEventHandler(AssemblyResolve);
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+
+
+        public static Assembly AssemblyResolve(object sender, ResolveEventArgs args)
+        {
+            if (args.Name.Contains(","))
+            {
+                string fileName = args.Name.Substring(0, args.Name.IndexOf(",")) + ".dll";
+                string assemblyPath = Application.StartupPath + @"/Core/" + fileName;
+
+                if (System.IO.File.Exists(assemblyPath))
+                {
+                    return Assembly.LoadFile(assemblyPath);
+                }
+            }
+
+            return null;
+        }
+    }
+}
